5a8d9469429443f7597ab3002754bc2faa292b4a
MrMrProgrammer <mr.mr.programmer@gmail.com>
2024-02-13 09:20:22 +0330

    Create the BaseApp application and add the front page of the site and add the template

--- OnlineShop/settings.py
+++ OnlineShop/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'accounts',
+    'BaseApp',
 ]
 
 MIDDLEWARE = [
@@ -121,6 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
