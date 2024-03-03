9d6af785429aafe32a034635b4c2aff521760521
hameddjf <99538932+hameddjf@users.noreply.github.com>
2024-03-03 10:21:09 +0330

    edited_admin_file

--- BaseApp/admin.py
+++ BaseApp/admin.py
@@ -3,6 +3,3 @@ from .models import Product, Category, Brand, Image
 
 # Register your models here.
 admin.site.register(Product, )
-admin.site.register(Category, )
-admin.site.register(Brand, )
-admin.site.register(Image, )
