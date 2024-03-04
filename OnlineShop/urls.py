b77b14cf9331b31239ef561f979d58630f175a4a
ERFAN <erfansafarzad7@gmail.com>
2024-03-04 12:50:01 +0330

    intro

--- OnlineShop/urls.py
+++ OnlineShop/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include("accounts.urls")),
-    path('', include("BaseApp.urls")),
+    path('', include(("BaseApp.urls", 'home'), namespace='home')),
 ]
