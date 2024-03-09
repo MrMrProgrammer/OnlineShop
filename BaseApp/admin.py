5a6a5ee509e6b4e9b49dc4bcab436a5aece8af5a
ERFAN <erfansafarzad7@gmail.com>
2024-03-09 13:52:24 +0330

    fixing-bugs

--- BaseApp/admin.py
+++ BaseApp/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.utils.html import format_html
 from django.utils.translation import gettext_lazy as _
-from .models import Base, Product, Category, Image, Brand, TypesFeature, ProductObject
+from .models import Base, Product, Category, OtherCategory, Image, Brand, ProductFeature, ProductObject
+from django import forms
+
 
 admin.site.site_header = 'فروشگاه اینترنتی'
 
@@ -9,12 +11,19 @@ admin.site.site_header = 'فروشگاه اینترنتی'
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('title', 'brand', 'public', 'image_tag')
+
     search_fields = ('title', 'brand__name', 'category__title')
+
     list_filter = ('category', 'brand', 'public')
+
     actions = ['make_public', 'make_private', 'duplicate_product']
+
+    prepopulated_fields = {'slug': ('title',), }
+
     readonly_fields = ('image_tag',)  # اضافه کردن image_tag به readonly_fields
+
     fieldsets = (
-        (None, {'fields': ('title', 'image', 'images', 'descriptions')}),  # حذف image_tag از اینجا
+        (None, {'fields': ('title', 'slug', 'image', 'images', 'descriptions')}),  # حذف image_tag از اینجا
         (_('متعلق به:'), {'fields': ('category', 'brand',)}),
         (_('نمایش عمومی:'), {'fields': ('public',)}),
     )
@@ -23,6 +32,7 @@ class ProductAdmin(admin.ModelAdmin):
         if obj.image:
             return format_html('<img src="{}" style="width: 45px; height:45px;" />', obj.image.url)
         return "-"
+    
     image_tag.short_description = _('Image')
 
     def make_public(self, request, queryset):
@@ -43,12 +53,17 @@ class ProductAdmin(admin.ModelAdmin):
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('title', 'parent', 'image_tag')
+
     search_fields = ('title', )
+
     list_filter = ('title', 'parent')
-    # prepopulated_fields = {'slug': ('title',)}
+
+    prepopulated_fields = {'slug': ('title',)}
+
     actions = ['duplicate_category']
+
     fieldsets = (
-        (_('دسته‌بندی ها:'), {'fields': ('parent', )}),
+        (_('دسته‌بندی ها:'), {'fields': ('parent', 'slug', )}),
         (('عنوان'), {'fields': ('title', 'image',)}),
     )
 
@@ -69,7 +84,9 @@ class CategoryAdmin(admin.ModelAdmin):
 @admin.register(Image)
 class ImageAdmin(admin.ModelAdmin):
     list_display = ('image_tag', 'image_file')
+
     readonly_fields = ('image_tag',)
+
     fieldsets = (
         (None, {'fields': ('image', )}),
     )
@@ -88,19 +105,48 @@ class ImageAdmin(admin.ModelAdmin):
     image_file.short_description = _('Image File Name')
 
 
+# get_cat = OtherCategory.objects.all()
+# ALL_OTHER_CATEGORIES = [i.title for i in zip(get_cat, get_cat)]
+# print(get_cat)
+# print(ALL_OTHER_CATEGORIES)
+#
+#
+# class ChooseCatForm(forms.Form):
+#     choice = forms.ChoiceField(label='all categories', choices=ALL_OTHER_CATEGORIES)
+#
+#
+# def add_to_other_categories(modeladmin, request, queryset):
+#     form = ChooseCatForm(request.POST)
+#     if form.is_valid():
+#         form_choice = form.cleaned_data['choice']
+#         filter_cat = get_cat.filter(title=form_choice)
+#         for obj in queryset:
+#             print(obj)
+#             obj.product.other_category.add(filter_cat)
+#             obj.save()
+
+
 @admin.register(ProductObject)
-class ObjectsAdmin(admin.ModelAdmin):
+class ProductObjectAdmin(admin.ModelAdmin):
     list_display = ('product', 'stock', 'price',
                     'discount', 'created', 'available')
+    
     list_filter = ('available', 'created', 'product')
+
     search_fields = ('product__title', )
+
     ordering = ('-created',)
+
+    # actions = (add_to_other_categories, )
+
+    readonly_fields = ('sold', 'users_recommend',)
+
     fieldsets = (
         (None, {
-            'fields': ('product', 'features', 'description')
+            'fields': ('slug', 'product', 'features', 'description')
         }),
         (_('Inventory'), {
-            'fields': ('stock', 'available'),
+            'fields': ('stock', 'available', 'sold'),
         }),
         (_('Pricing'), {
             'fields': ('price', 'discount'),
@@ -116,5 +162,12 @@ class ObjectsAdmin(admin.ModelAdmin):
         return super().formfield_for_foreignkey(db_field, request, **kwargs)
 
 
-admin.site.register(TypesFeature, )
-admin.site.register(Brand, )
+@admin.register(Brand)
+class ImageAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+
+    fieldsets = (
+        (None, {'fields': ('image', )}),
+    )
+
+admin.site.register(ProductFeature, )
