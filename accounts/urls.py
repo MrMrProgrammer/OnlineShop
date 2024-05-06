b97375f883345e29b7f8572c6f3442703c32684f
barzan1380 <131408916+barzan1380@users.noreply.github.com>
2024-05-07 00:57:41 +0330

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -19,3 +19,14 @@ urlpatterns = [
 
 
 ]
+#------------
+from django.urls import path
+from .views import *
+
+app_name = 'accounts'
+
+urlpatterns = [
+    path('register/', register, name='register'),
+    path('login/', user_login, name='login'),
+    path('logout/', user_logout, name='logout')
+]
