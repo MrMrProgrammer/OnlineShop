64ee3cd75ce6e538e46eaec90f4857415ef43c46
barzan1380 <131408916+barzan1380@users.noreply.github.com>
2024-05-15 01:10:03 +0330

    Update models.py

--- ProductObject/models.py
+++ ProductObject/models.py
@@ -84,3 +84,64 @@ class Promotion(models.Model):
     class Meta:
         verbose_name = _("تخفیف & مناسبت")
         verbose_name_plural = _("تخفیفات & مناسبتها")
+#------------------------new 5/15from django.db import models
+from accounts.models import ShopUser
+from django.urls import reverse
+
+
+# Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=50, blank=True, unique=True)
+    slug = models.SlugField(unique=True, max_length=100)
+    image = models.ImageField(upload_to='category_images/', null=True, blank=True)
+
+    class Meta:
+        verbose_name = "دسته بندی"
+        verbose_name_plural = "دسته بندی ها"
+
+    def __str__(self):
+        return self.title if self.title else 'None'
+
+
+class Brand(models.Model):
+    title = models.CharField(max_length=50, blank=True, unique=True)
+    slug = models.SlugField(unique=True, max_length=100)
+    image = models.ImageField(upload_to='brand_images/', null=True, blank=True)
+
+    class Meta:
+        verbose_name = 'برند'
+        verbose_name_plural = 'برند'
+
+    def __str__(self):
+        return self.title if self.title else 'None'
+
+
+class Product(models.Model):
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE, related_name='products')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200)
+    description = models.TextField(max_length=2000)
+    inventory = models.PositiveIntegerField(default=0)
+    weight = models.PositiveIntegerField(default=0)
+    price = models.PositiveIntegerField(default=0)
+    off = models.PositiveIntegerField(default=0)
+    new_price = models.PositiveIntegerField(default=0)
+    likes = models.ManyToManyField(ShopUser, related_name='liked_products', blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created']
+        indexes = [
+            models.Index(fields=['id', 'slug']),
+            models.Index(fields=['name']),
+            models.Index(fields=['-created'])
+        ]
+
+    def get_absolute_url(self):
+        return reverse('shop:product_detail', args=[self.pk, self.slug])
+
+    def __str__(self):
+        return self.name
+
